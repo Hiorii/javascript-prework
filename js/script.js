1be2c1beca11ba4aeb0591b1b3c52ab32ce2afe4
Bartosz Janiak <janiak.bartosz92@gmail.com>
2020-06-02 20:43:58 +0200

    Add player move

--- js/script.js
+++ js/script.js
@@ -6,6 +6,26 @@ let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
   computerMove = 'kamień';
+}else if (randomNumber == 2){
+    computerMove = 'papier';
+}else {
+    computerMove = 'nożyce'
 }
 
-printMessage('Mój ruch to: ' + computerMove);
+printMessage('Ruch komputera to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}else if(playerInput == '2'){
+    playerMove = 'papier'
+}else {
+    playerMove = 'nożyce'
+}
+
+printMessage('Ruch gracza to: ' + playerMove);
