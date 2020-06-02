ec0e3b9413258223cd4ef6ac92ea94ad31c64c6e
Bartosz Janiak <janiak.bartosz92@gmail.com>
2020-06-02 21:01:09 +0200

    Finish base version of game

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ if(randomNumber == 1){
   computerMove = 'kamień';
 }else if (randomNumber == 2){
     computerMove = 'papier';
-}else {
+}else if (randomNumber == 3){
     computerMove = 'nożyce'
 }
 
@@ -24,8 +24,30 @@ if(playerInput == '1'){
   playerMove = 'kamień';
 }else if(playerInput == '2'){
     playerMove = 'papier'
-}else {
+}else if (playerInput == '3'){
     playerMove = 'nożyce'
-}
+} 
 
 printMessage('Ruch gracza to: ' + playerMove);
+
+if (playerMove == 'nieznany ruch'){
+    printMessage('You chose incorrect number');
+} else if(playerMove == 'papier' && computerMove == 'papier'){
+    printMessage('remis');
+} else if(playerMove == 'kamień' && computerMove == 'kamień'){
+    printMessage('remis');
+} else if(playerMove == 'nożyce' && computerMove == 'nożyce'){
+    printMessage('remis');
+} else if (playerMove == 'papier' && computerMove == 'nożyce'){
+    printMessage('Computer Won!')
+} else if (playerMove == 'papier' && computerMove == 'kamień'){
+    printMessage('You Won!')
+} else if (playerMove == 'kamień' && computerMove == 'papier'){
+    printMessage('Computer Won!')
+} else if (playerMove == 'kamień' && computerMove == 'nożyce'){
+    printMessage('You Won!')
+} else if (playerMove == 'nożyce' && computerMove == 'kamień'){
+    printMessage('Computer Won!')
+} else if (playerMove == 'nożyce' && computerMove == 'papier'){
+    printMessage('You Won!')
+}
